af5c460ebf075c9c2007c8bc99e37cebdb87ba91
THORsalo0 <petrobokalo16@gmail.com>
2023-09-11 22:34:24 +0300

    Test2
    12fddf

---
+++ Java_labs/src/Hello.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Hello
+{
+public static void main(String[] args)
+{
+String message = new String();
+message = "Hello, Java";
+System.out.println(message);
+}
+}
